bc9425d099e785194e09ec4557f4c1fcaf783f5c
Falk Röder <mail@falk-roeder.de>
2018-02-10 20:22:00 +0100

    Added Example Sitemap Config for extensions for cs_seo

--- Configuration/TypoScript/Seo/Sitemap.ts
+++ Configuration/TypoScript/Seo/Sitemap.ts
@@ -2,7 +2,7 @@ lib.news = COA
 lib.news {
     table = tx_news_domain_model_news
     # dieser Parameter wird mit in der URL mit der UID verknüpft, davor sind mehr Parameter
-    additionalParams = tx_news_pi1[controller]=News&tx_news_pi1[action]=eventDetail&tx_news_pi1[news]
+    additionalParams = tx_news_pi1[controller]=News&tx_news_pi1[action]=detail&tx_news_pi1[news]
     # Seiten-ID, wo die Datensätze im Backend gespeichert sind
     #storagePid = 205,209,314
     # Seiten-ID wo einzelne Datensätze im Frontend angezeigt werden. Pflichtangabe!
@@ -21,31 +21,14 @@ lib.news {
     categoryMMFieldname = categories
 }
 plugin.tx_csseo.sitemap {
-    pages.languageUids = 0,1
+    pages.languageUids = 0
     extensions {
-        news >
+#        news >
         # configuration for an extension
-       #aktuelles < lib.news
-       #aktuelles.storagePid = 205,207,208,209,210,211,212
-       #aktuelles.detailPid = 199
-       #treffensieuns < lib.news
-       #treffensieuns.storagePid = 314
-       #treffensieuns.detailPid = 366
-       #memoqschulung < lib.news
-       #memoqschulung.storagePid = 278
-       #memoqschulung.detailPid = 280
-       #memsourceschulung < lib.news
-       #memsourceschulung.storagePid = 310
-       #memsourceschulung.detailPid = 301
-       #multitermschulung < lib.news
-       #multitermschulung.storagePid = 313
-       #multitermschulung.detailPid = 303
-       #passolo < lib.news
-       #passolo.storagePid = 312
-       #passolo.detailPid = 305
-       #trados < lib.news
-       #trados.storagePid = 311
-       #trados.detailPid = 306
+        datednews < lib.news
+        datednews.storagePid = 12
+        datednews.detailPid = 15
+
 
     }
 }
